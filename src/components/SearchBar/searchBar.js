0cc1c9b4ef686bef3a4d42086ec30f313c19807b
Adam Heeps <adam.heeps@live.co.uk>
2022-01-14 19:29:29 +0000

    set up call to Google API for geocoding to get lat and lon

--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -4,10 +4,19 @@ import { useState } from 'react';
 const SearchBar = () => {
 
     const [resData, setResData] = useState([])
+    const [lat, setLat] = useState()
+    const [lon, setLon] = useState()
+    
+    const handleSubmit = (location) => {
+        location.preventDefault()
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
+        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyA3mAQaQEIWHzmo5vMI6qIoCYAUUjLeAE4`)
+        .then((res) => {
+          setLat(res.data.results[0].geometry.location.lat)
+          setLon(res.data.results[0].geometry.location.lng)
+        })
+
+        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
         .then((res) => {
             // toFixed rounds the number to specified floating point value. 
             // -273.15 is the formula for the Kelvin to Celcius conversion.
