f30133c4905a3b51dc498af1952c86cd6602d3f6
Adam Heeps <adam.heeps@live.co.uk>
2022-01-14 19:05:58 +0000

    form to get current default weather

--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -5,21 +5,25 @@ const SearchBar = () => {
 
     const [resData, setResData] = useState([])
 
-    const handleSearch = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
+        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
         .then((res) => {
-          setResData(res.data)
-          console.log(res.data)
-        })
+            // toFixed rounds the number to specified floating point value. 
+            // -273.15 is the formula for the Kelvin to Celcius conversion.
+            setResData((res.data.current.temp-273.15).toFixed(0)) 
+            console.log(res.data)
+        }) 
     }
 
 
     return (
         <div className="App-header">
-            <h1> This is a search bar</h1>
-            <button onClick={handleSearch}></button>
-            {resData}
+            <form onSubmit={handleSubmit}> 
+                <input placeholder="Enter a city..." id="input-search"></input>
+                <button type="submit" id="submit-search" > Search </button>
+            </form>
+            Current temperature: {resData} C
         </div>
     )
 }
