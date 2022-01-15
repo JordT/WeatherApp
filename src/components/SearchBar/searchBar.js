c5b7fda927027be72199d0e515d4c1fb4be32244
Adam Heeps <adam.heeps@live.co.uk>
2022-01-15 10:14:15 +0000

    bug fixed where state is not rendering

--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const SearchBar = () => {
 
     const [resData, setResData] = useState([])
-    const [lat, setLat] = useState()
-    const [lon, setLon] = useState()
     const [location, setLocation] = useState('Monaco')
 
     const handleSubmit = (e) => {
@@ -14,24 +12,20 @@ const SearchBar = () => {
         // Calling the GCP geocoding API to get lat/lon values.
         axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyA3mAQaQEIWHzmo5vMI6qIoCYAUUjLeAE4`)
         .then((res) => {
-          setLat(res.data.results[0].geometry.location.lat)
-          setLon(res.data.results[0].geometry.location.lng)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-  
-        // Taking the GCP values and hitting the OpenWeather API to retreive weather data.
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
-        .then((res) => {
-            // toFixed rounds the number to specified floating point value. 
-            // -273.15 is the formula for the Kelvin to Celcius conversion.
-            setResData((res.data.current.temp-273.15).toFixed(0)) 
-            console.log(res.data)
-        }) 
-        .catch((err) => {
-            console.log(err);
-        });
+          const lat = res.data.results[0].geometry.location.lat
+          const lon = res.data.results[0].geometry.location.lng
+          // Taking the GCP values and hitting the OpenWeather API to retreive weather data.
+          axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=508459cf94bd056fe8b95a12ab1ac1e7`)
+            .then((res) => {
+                // toFixed rounds the number to specified floating point value. 
+                // -273.15 is the formula for the Kelvin to Celcius conversion.
+                setResData((res.data.current.temp-273.15).toFixed(0)) 
+                console.log(res.data)
+            }) 
+            .catch((err) => {
+                console.log(err);
+            });
+        })       
     }
 
     return (
