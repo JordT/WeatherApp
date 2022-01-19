eda1624acaf4d49fce377000fba8643faf8457d3
Jordan Thomson <jm.thomson92@gmail.com>
2022-01-19 15:30:11 +0000

    started sunposition solution

--- src/components/DisplayWeather/SunPosition/SunPosition.js
+++ src/components/DisplayWeather/SunPosition/SunPosition.js
@@ -2,11 +2,26 @@ import './SunPosition.css'
 
 const SunPosition = (sunData) => {
 //   console.log(sunData.sunrise)
-  return (
+
+// define 12 positions?
+const sunPos = (x) => {
+  switch(x) {
+    case 1:
+      //
+  }
+}
+
+return (
     //   <h1>{sunData}</h1>
       <div className="sun-pos-container">
          <div className="arc">
-            <div className="sun"></div>
+           <div className="sun-container"
+            style={{
+              left: '95%', // change these to move sun pos
+              top: '85%'  // change these to move sun pos
+              }}>
+              <div className="sun"></div> 
+           </div>
         </div>
          {/* <h1>SunPosition</h1> */}
       </div>
