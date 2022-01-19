ce49301a3facbf32974ca3c7dfbda51cb7c12450
Adam Heeps <adam.heeps@live.co.uk>
2022-01-19 18:43:51 +0000

    formatted css for current card

--- src/components/DisplayWeather/NextFiveDays/NextFiveDays.js
+++ src/components/DisplayWeather/NextFiveDays/NextFiveDays.js
@@ -8,13 +8,14 @@ const NextFiveDays = (props) => {
         
         return (
             <div key='current' id="current-card" className="card">
+                    <p1 className="day-name">Current</p1><br/>
                     <p1>{w.weather[0].description}</p1><br/>
                     <img
                     className="weather-icon" 
                     src={`http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`}                   
                     alt="new"
                     /><br/>
-                    <p1>Current: {w.temp.toFixed(0)}C</p1>
+                    <p1>{w.temp.toFixed(0)}C</p1>
             </div>
         )
     }
