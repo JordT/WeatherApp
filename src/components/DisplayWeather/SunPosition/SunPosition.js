c9f8d719125efd7236f3cdba50132f2de4f5b847
Jordan Thomson <jm.thomson92@gmail.com>
2022-01-19 16:11:30 +0000

    sun positions set

--- src/components/DisplayWeather/SunPosition/SunPosition.js
+++ src/components/DisplayWeather/SunPosition/SunPosition.js
@@ -1,24 +1,62 @@
 import './SunPosition.css'
 
 const SunPosition = (sunData) => {
-//   console.log(sunData.sunrise)
+
+// get Sun position from sunrise + sundown information, then get that to 1-9 figure, pass that into sunPos
 
 // define 12 positions?
+let leftPos = 0;
+let topPos = 0;
+
 const sunPos = (x) => {
   switch(x) {
     case 1:
-      //
+      leftPos = -7.5;
+      topPos = 80;
+      break;
+    case 2:
+      leftPos = -6;
+      topPos = 60;
+      break;
+    case 3:
+      leftPos = 5;
+      topPos = 20;
+      break;
+    case 4:
+      leftPos = 20;
+      topPos = -5;
+      break;
+    case 5:
+      leftPos = 44;
+      topPos = -15;
+      break;
+    case 6:
+      leftPos = 64;
+      topPos = -5;
+      break;
+    case 7:
+      leftPos = 80;
+      topPos = 20;
+      break;
+    case 8:
+      leftPos = 91;
+      topPos = 60;
+      break;
+    case 9:
+      leftPos = 93;
+      topPos = 80;
+      break;
   }
 }
-
+sunPos(8)
 return (
     //   <h1>{sunData}</h1>
       <div className="sun-pos-container">
          <div className="arc">
            <div className="sun-container"
             style={{
-              left: '95%', // change these to move sun pos
-              top: '85%'  // change these to move sun pos
+              left: `${leftPos}%`, // change these to move sun pos
+              top: `${topPos}%`  // change these to move sun pos
               }}>
               <div className="sun"></div> 
            </div>
