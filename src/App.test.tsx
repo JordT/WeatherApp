8049a02e00fdd9ccf19bb7c40da671b5f5d0149a
Adam Heeps <adam.heeps@live.co.uk>
2022-01-31 20:34:49 +0000

    current weather tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -13,15 +13,27 @@ afterEach(() => {
 
 describe('App tests', () => {
   
-    test('renders App component', () => {
+    test('App component', () => {
     // Test data json loaded up with Monaco weather @ 6:03 PM
     expect(screen.getByText(/The local time in Monaco is 6:03 PM/)).toBeInTheDocument();
   });
 
-  test('search bar rendered and populated with Monaco', () => {
+  test('search bar component', () => {
     expect(screen.getByDisplayValue(/Monaco/)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/Enter a city.../)).toBeInTheDocument();
-    expect(screen.getByRole('combobox'));
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+  })
+  
+  test('current weather component', () => {
+    expect(screen.getByText(/Current Weather/)).toBeInTheDocument();
+    expect(screen.getByText(/Temperature/)).toBeInTheDocument();
+    expect(screen.getByText(/Feels like/)).toBeInTheDocument();
+    expect(screen.getByText(/Wind speed/)).toBeInTheDocument();
+    expect(screen.getByText(/Wind gust/)).toBeInTheDocument();
+    expect(screen.getByText(/Wind direction/)).toBeInTheDocument();
+    expect(screen.getByText(/Humidity/)).toBeInTheDocument();
+    expect(screen.getByAltText('current-weather-icon')).toBeInTheDocument();
+    
   })
 
   
