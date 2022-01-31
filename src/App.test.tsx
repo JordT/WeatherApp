eeca716c820ac02b61991639b532a5ca81d6c366
Adam Heeps <adam.heeps@live.co.uk>
2022-01-31 20:20:06 +0000

    search bar tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,25 @@ import '@testing-library/jest-dom'
 
 import App from './App';
 
-describe('App', () => {
-  test('renders App component', () => {
-    render(<App />);
+beforeEach(() => {
+  render(<App />);
+})
 
-    expect(screen.getByText('The local time in Monaco is 6:03 PM')).toBeInTheDocument();
+afterEach(() => {
+})
+
+describe('App tests', () => {
+  
+    test('renders App component', () => {
+    // Test data json loaded up with Monaco weather @ 6:03 PM
+    expect(screen.getByText(/The local time in Monaco is 6:03 PM/)).toBeInTheDocument();
   });
+
+  test('search bar rendered and populated with Monaco', () => {
+    expect(screen.getByDisplayValue(/Monaco/)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Enter a city.../)).toBeInTheDocument();
+    expect(screen.getByRole('combobox'));
+  })
+
+  
 })
