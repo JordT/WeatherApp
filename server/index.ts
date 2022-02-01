9e4d3286e5e1deb408912a54e9965f86518c7370
Adam Heeps <adam.heeps@live.co.uk>
2022-02-01 21:06:28 +0000

    express server now integrated to show weather

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import controller from './index.controller'
 
 const app = express()
 const port = 4000
+const cors = require("cors");
 
-app.get('/api/geocode', controller.getCoordinatesFromLocation)
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
-app.get('/api/weather', controller.getWeatherData)
+app.get('/api/weather/:location', controller.getWeatherData)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
