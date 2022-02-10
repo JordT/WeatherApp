dbdef51b1d4c856304e6bf272839a8962f031e32
Adam Heeps <adam.heeps@live.co.uk>
2022-02-10 19:49:40 +0000

    some housekeeping completed.

--- server/index.controller.ts
+++ server/index.controller.ts
@@ -8,13 +8,6 @@ const getWeatherData = (req: Request, res: Response) => {
   let formattedAddress: string = location
   let lat: string = req.params.lat
   let lon: string = req.params.lon
-
-  // for (let j = 0; j < cities.length; j++) {
-  //   if (cities[j].name +', ' + cities[j].country === location) {
-  //     lat = cities[j].lat
-  //     lon = cities[j].lng
-  //     break;
-  // }}
     
     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${process.env.WEATHER_API_KEY}`)
     .then(r => {
